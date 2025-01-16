2c42014e25cf733e95f4497081de974e30d6f90b
Nikos Gavalakis <nikosgavalakis@gmail.com>
2025-01-16 10:02:41 +0200

    Add files via upload

---
+++ our_doctors_n.php
@@ -0,0 +1,52 @@
+<?php include('up.php'); ?>
+
+<?php
+
+$quer="SELECT * FROM user WHERE role='Doctor' ";
+$res = mysqli_query($con, $quer);
+$row = mysqli_fetch_assoc($res);
+if($row){	
+// Εκτέλεση query για να πάρουμε τα δεδομένα από τους δύο πίνακες
+$query = "
+    SELECT 
+		u.name,
+        u.email, 
+        u.eidikotita, 
+        d.fb_site, 
+        d.linkin, 
+        d.site, 
+        d.doxy_site
+    FROM 
+        user u 
+    INNER JOIN 
+        doctors d 
+    ON 
+        u.id_user = d.doctor_id ";
+
+$result = mysqli_query($con, $query);
+
+if(mysqli_num_rows($result) > 0){
+    echo "<table border='1'>";
+    echo "<tr><th>Όνομα</th><th>Email</th><th>Ειδικότητα</th><th>Facebook</th><th>LinkedIn</th><th>Website</th><th>Doxy.me</th></tr>";
+	while($data = mysqli_fetch_assoc($result)){
+		echo "<tr>";
+		echo "<td>".$data['name']."</td>";
+		echo "<td>".$data['email']."</td>";
+		echo "<td>".$data['eidikotita']."</td>";
+
+		// Έλεγχος για κάθε link πριν δημιουργηθεί το κελί
+		echo "<td>".(!empty($data['fb_site']) ? "<a href='".$data['fb_site']."'>Facebook</a>" : "")."</td>";
+		echo "<td>".(!empty($data['linkin']) ? "<a href='".$data['linkin']."'>LinkedIn</a>" : "")."</td>";
+		echo "<td>".(!empty($data['site']) ? "<a href='".$data['site']."'>Website</a>" : "")."</td>";
+		echo "<td>".(!empty($data['doxy_site']) ? "<a href='".$data['doxy_site']."'>Doxy.me</a>" : "")."</td>";
+		echo "</tr>";
+	}
+    echo "</table>";
+}else{
+    echo "<p>Δεν βρέθηκαν δεδομένα.</p>";
+}
+}
+
+// Κείμενο κάτω από τον πίνακα
+echo "<p>Ο παραπάνω πίνακας περιλαμβάνει τα στοιχεία των γιατρών και τους σχετικούς συνδέσμους. Αν κάποιο πεδίο είναι κενό, δεν εμφανίζεται το αντίστοιχο link.</p>";
+?>
