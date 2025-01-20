7c45c2c560275e98e5b7dd0ba6b85fbd31d1b225
Nikos Gavalakis <nikosgavalakis@gmail.com>
2025-01-20 13:10:12 +0200

    Add files via upload

--- your_doctor.php
+++ your_doctor.php
@@ -50,23 +50,19 @@ if ($result && mysqli_num_rows($result) > 0) {
         $site = htmlspecialchars($row['site'], ENT_QUOTES, 'UTF-8');
         $doxy_site = htmlspecialchars($row['doxy_site'], ENT_QUOTES, 'UTF-8');
 
-        while($data = mysqli_fetch_assoc($result)){
-			echo "<tr>";
-			echo "<td>".$data['name']."</td>";
-			echo "<td>".$data['email']."</td>";
-			echo "<td>".$data['eidikotita']."</td>";
-
-			// Έλεγχος για κάθε link πριν δημιουργηθεί το κελί
-			echo "<td>".(!empty($data['fb_site']) ? "<a href='".$data['fb_site']."' target='_blank'><button>Facebook</button></a>" : "")."</td>";
-			echo "<td>".(!empty($data['linkin']) ? "<a href='".$data['linkin']."' target='_blank'><button>LinkedIn</button></a>" : "")."</td>";
-			echo "<td>".(!empty($data['site']) ? "<a href='".$data['site']."' target='_blank'><button>Website</button></a>" : "")."</td>";
-			echo "<td>".(!empty($data['doxy_site']) ? "<a href='".$data['doxy_site']."' target='_blank'><button>Doxy.me</button></a>" : "")."</td>";
-			echo "</tr>";
-		}
-		echo "</table><br><br>";
+        echo "
+			<tr>
+				<td>$name</td>
+                <td>$email</td>
+                <td>$eidikotita</td>
+                <td><a href='$fb_site' target='_blank'><button>Facebook</button></a></td>
+                <td><a href='$linkin' target='_blank'><button>LinkedIn</button></a></td>
+                <td><a href='$site' target='_blank'><button>Ιστοσελίδα</button></a></td>
+                <td><a href='$doxy_site' target='_blank'><button>Doxy</button></a></td>
+              </tr>";
     }
 
-    
+    echo "</table><br><br>";
 	echo "  <h3 style='color: green;'><strong > Από τα άνωθεν Links: </strong></h3>
 			<h4>Έχεις την δυνατότητα να στείλεις e-mail στον γιατρό σου.</h4>
 			<h4>Μπορείς να δείς τις σελίδες του από τα κοινωνικά και επαγγελματικά του δίκτυα, εφόσον τις έχει καταχωρήσει.</h4>
