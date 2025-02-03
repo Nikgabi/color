5da82c2f1f303bb1e8937f2715d016fd3afccd61
Nikos Gavalakis <nikosgavalakis@gmail.com>
2025-02-03 11:15:12 +0200

    Add files via upload

--- anxiety_score.php
+++ anxiety_score.php
@@ -57,7 +57,6 @@
 			$psyc_id = mysqli_real_escape_string($con, $psyc_id);
 			$scor = mysqli_real_escape_string($con, $scor);
 			$message = mysqli_real_escape_string($con, $message);
-			var_dump($psyc_id, $scor, $message);
 			$query = "INSERT INTO pcyc_data (psyc_id, scor_stress, message_stress) 
           VALUES('$psyc_id', '$scor', '$message')";
 
