0033ed96eaf4f19b57f5e8b5345612c98a1c5374
Nikos Gavalakis <nikosgavalakis@gmail.com>
2025-04-07 10:20:18 +0300

    Add files via upload

--- down.php
+++ down.php
@@ -1,23 +1,21 @@
 <!-- footer section -->
+
   <footer class="footer_section">
-	
-  
-  
     <div class="container">
       <p>
 	   <span id="displayYear" hidden></span><!-- All Rights Reserved By
         <a href="https://html.design/">Free Html Templates</a><br>-->
-        <a  href="rules.php">ΟΡΟΙ</a>
+        <a  href="<?php echo BASE_URL; ?>auxi/rules.php">ΟΡΟΙ</a>
 		---
-        <a  href="policy.php">ΠΟΛΙΤΙΚΗ ΑΠΟΡΡΗΤΟΥ</a>
+        <a  href="<?php echo BASE_URL; ?>auxi/policy.php">ΠΟΛΙΤΙΚΗ ΑΠΟΡΡΗΤΟΥ</a>
 		---
-		<a  href="instructions.php">ΟΔΗΓΙΕΣ</a>
+		<a  href="<?php echo BASE_URL; ?>auxi/instructions.php">ΟΔΗΓΙΕΣ</a>
 		---
-		<a  href="bibliography.php" >ΒΙΒΛΙΟΓΡΑΦΙΑ</a>
+		<a  href="<?php echo BASE_URL; ?>bibliography.php" >ΒΙΒΛΙΟΓΡΑΦΙΑ</a>
 		---
-		<a  href="testimonials.php" >TESTIMONIALS</a>
+		<a  href="<?php echo BASE_URL; ?>testimonials.php" >TESTIMONIALS</a>
 		---
-		<a  href="about.php" >ABOUT</a>
+		<a  href="<?php echo BASE_URL; ?>about.php" >ABOUT</a>
 		---
 		<small>&copy; Copyright 2025 All Rights Reserved by ygeiafirst.net</small>
       </p>
@@ -30,7 +28,7 @@
 
   
   <!-- bootstrap js -->
-  <script src="js/bootstrap.js"></script>
+  <script src="<?php echo BASE_URL; ?>js/bootstrap.js"></script>
   <!-- nice select -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-nice-select/1.1.0/js/jquery.nice-select.min.js" integrity="sha256-Zr3vByTlMGQhvMfgkQ5BtWRSKBGa2QlspKYJnkjZTmo=" crossorigin="anonymous"></script>
   <!-- owl slider -->
@@ -38,5 +36,5 @@
   <!-- datepicker -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/js/bootstrap-datepicker.js"></script>
   <!-- custom js -->
-  <script src="js/custom.js"></script>
+  <script src="<?php echo BASE_URL; ?>js/custom.js"></script>
   </html>
--- up.php
+++ up.php
@@ -2,243 +2,131 @@
 <?php
 	session_start();
 	include ('connection.php');
-	?>	
+?>	
 <html>
 <head>
-  <!-- Basic -->
   <meta charset="utf-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <!-- Mobile Metas -->
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-  <!-- Site Metas -->
   <meta name="keywords" content="Medecine , Doctors, Medical functions , ΕΑΠ , πτυχιακή" />
-  <meta name="description" content="Διαδικτυακή πλατφόρμα ιατρικών υπολογισμών, τηλειατρικής και 
-  πληροφοριών τεχνητής νοημοσύνης στα πλαίσια πτυχιακής εργασίας στο Ελληνικό Ανοικτό Πανεπιστήμιο " />
+  <meta name="description" content="Διαδικτυακή πλατφόρμα ιατρικών υπολογισμών, τηλειατρικής και πληροφοριών τεχνητής νοημοσύνης στα πλαίσια πτυχιακής εργασίας στο Ελληνικό Ανοικτό Πανεπιστήμιο " />
   <meta name="author" content="Gavalakis Nikos" />
-  <!-- Favicon -->
-  <link rel="icon" type="image/png" href="images/favicon.png">
-  <!-- jQery -->
-  <script src="js/jquery-3.7.1.min.js"></script>
-  <!-- bootstrap core css -->
-  <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
-  <!-- fonts style -->
-  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet">
-  <!--owl slider stylesheet -->
-  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
-  <!-- font awesome style -->
-  <link href="css/font-awesome.min.css" rel="stylesheet" />
-  <!-- nice select -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-nice-select/1.1.0/css/nice-select.min.css" integrity="sha256-mLBIhmBvigTFWPSCtvdu6a76T+3Xyt+K571hupeFLg4=" crossorigin="anonymous" />
-  <!-- datepicker -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/css/datepicker.css">
-  <!-- Custom styles for this template -->
-  <link href="css/style.css" rel="stylesheet" />
-  <link href="blog.css" rel="stylesheet" />
-  <!-- responsive style -->
-  <link href="css/responsive.css" rel="stylesheet" />
-  
-  <!-- Google tag (gtag.js) -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=G-WJZGQ8EM5K"></script>
-	<script>
-	  window.dataLayer = window.dataLayer || [];
-	  function gtag(){dataLayer.push(arguments);}
-	  gtag('js', new Date());
+  <link rel="icon" type="image/png" href="<?php echo BASE_URL; ?>images/favicon.png">
 
-	  gtag('config', 'G-WJZGQ8EM5K');
-	</script>
+  <!-- Scripts & CSS -->
+  <script src="<?php echo BASE_URL; ?>js/jquery-3.7.1.min.js"></script>
+  <link rel="stylesheet" href="<?php echo BASE_URL; ?>css/bootstrap.css" />
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+  <link href="<?php echo BASE_URL; ?>css/font-awesome.min.css" rel="stylesheet" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-nice-select/1.1.0/css/nice-select.min.css" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/css/datepicker.css">
+  <link href="<?php echo BASE_URL; ?>css/style.css" rel="stylesheet" />
+  <link href="<?php echo BASE_URL; ?>blog.css" rel="stylesheet" />
+  <link href="<?php echo BASE_URL; ?>css/responsive.css" rel="stylesheet" />
+
+  <!-- Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-WJZGQ8EM5K"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-WJZGQ8EM5K');
+  </script>
 </head>
 
 <body class="sub_page">
   <div class="hero_area">
-    <!-- header section strats -->
+    <!-- header section -->
     <header class="header_section">
       <div class="header_top">
         <div class="container">
           <div class="contact_nav">
             <a href="#">
-              <i class="fa fa-phone" aria-hidden="true"></i>
-              <span>
-                Call :6944818841
-              </span>
+              <i class="fa fa-phone"></i><span>Call :6944818841</span>
             </a>
             <a href="mailto:info@ygeiafirst.net">
-              <i class="fa fa-envelope" aria-hidden="true"></i>
-              <span>
-                Email :info@ygeiafirst.net
-              </span>
+              <i class="fa fa-envelope"></i><span>Email :info@ygeiafirst.net</span>
             </a>
-            <a href="location.php">
-              <i class="fa fa-map-marker" aria-hidden="true"></i>
-              <span>
-                Location
-              </span>
+            <a href="<?php echo BASE_URL; ?>auxi/location.php">
+              <i class="fa fa-map-marker"></i><span>Location</span>
             </a>
-			</a>
-		<!--	<a href="testimonials.php">
-              <i class="fa fa-thumbs-o-up" aria-hidden="true"></i>
-              <span>
-                Testimonials
-              </span>
-            </a>-->
-			<a href="pdf_helping.php">
-              <i class="fa fa-folder-open-o" aria-hidden="true"></i>
-              <span>
-                pdf_help
-              </span>
+            <a href="<?php echo BASE_URL; ?>auxi/pdf_helping.php">
+              <i class="fa fa-folder-open-o"></i><span>pdf_help</span>
             </a>
-			<a href="normal_values.php">
-              <i class="fa fa-wheelchair" aria-hidden="true"></i>
-              <span>
-                Φυσ_τιμές
-              </span>
+            <a href="<?php echo BASE_URL; ?>auxi/normal_values.php">
+              <i class="fa fa-wheelchair"></i><span>Φυσ_τιμές</span>
             </a>
-			<a href="opinion.php">
-              <i class="fa fa-star-o" aria-hidden="true"></i>
-              <span>
-                your_opinion
-              </span>
+            <a href="<?php echo BASE_URL; ?>auxi/opinion.php">
+              <i class="fa fa-star-o"></i><span>your_opinion</span>
             </a>
-			<?php 
-			if ((isset($_SESSION['email']) && isset($_SESSION['role'])) && $_SESSION['role'] == 'Doctor'){
-				echo '
-				<a href="form_doctor.php">
-				  <i class="fa fa-user-md" aria-hidden="true"></i>
-				  <span>
-					for_doctors
-				  </span>
-				</a>';}
-			?>
+            <?php 
+              if ((isset($_SESSION['email']) && isset($_SESSION['role'])) && $_SESSION['role'] == 'Doctor') {
+                echo '
+                <a href="' . BASE_URL . 'form_doctor.php">
+                  <i class="fa fa-user-md"></i><span>for_doctors</span>
+                </a>';
+              }
+            ?>
           </div>
         </div>
       </div>
+
       <div class="header_bottom">
         <div class="container-fluid">
-          <nav class="navbar navbar-expand-lg custom_nav-container ">
-            <a class="navbar-brand" href="#">
-              <img src="images/health_f.png" alt="">
+          <nav class="navbar navbar-expand-lg custom_nav-container">
+            <a class="navbar-brand" href="<?php echo BASE_URL; ?>">
+              <img src="<?php echo BASE_URL; ?>images/health_f.png" alt="">
             </a>
-            
 
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class=""> </span>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
+              <span class=""></span>
             </button>
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <div class="d-flex mr-auto flex-column flex-lg-row align-items-center">
-                <ul class="navbar-nav  ">
-                  <li class="nav-item ">
-                    <a class="nav-link " href="index.php">Home</a> 
-                  </li>
-				  <li class="nav-item">
-                    <a class="nav-link " href="calculation_istor.php">ιστορικο</a>
-                  </li>
-				  <li class="nav-item">
-                    <a class="nav-link " href="calculation.php">υπολογισμοι</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link " href="blog.php"> αρθρα</a>
-                  </li>
-                 <!-- <li class="nav-item">
-                    <a class="nav-link " href="bibliography.php">Βιβλιογραφια</a>
-                  </li>-->
-                  <li class="nav-item active">
-                    <a class="nav-link " href="doctors.php">Doctors</a>
-                  </li>
-				  <li class="nav-item active">
-                    <a class="nav-link " href="charts.php">Διαγραμματα</a>
-                  </li>
-				  <!--<li class="nav-item">
-                    <a class="nav-link " href="testimonials.php">αποψεις</a>
-                  </li>-->
-				  <?php
-					if (isset($_SESSION['consultant']) && $_SESSION['consultant'] !== '0' && !empty($_SESSION['consultant'])) {
-						
-							echo '
-							<li class="nav-item">
-								<a class="nav-link" href="your_doctor.php">Ο ΓΙΑΤΡΟΣ ΣΟΥ</a>
-							</li>';
-						} 
-					 else if ((isset($_SESSION['email']) && isset($_SESSION['role'])) && $_SESSION['role'] == 'visitor') {
-						// Αν ο χρήστης είναι επισκέπτης
-						echo '
-						<li class="nav-item">
-							<a class="nav-link " href="choose_doctor.php">ΔΙΑΛΕΞΕ ΓΙΑΤΡΟ</a>
-						</li>';
-					} else if ((isset($_SESSION['email']) && isset($_SESSION['role'])) && $_SESSION['role'] == 'Doctor') {
-						// Αν ο χρήστης είναι γιατρός
-						echo '
-						<li class="nav-item">
-							<a class="nav-link " href="teleconfer_con.php?consultant='. $_SESSION['id_user'].'">ΤΗΛΕΙΑΤΡΙΚΗ</a>
-						</li>';
-					} else {
-						// Αν δεν ικανοποιείται καμία από τις παραπάνω συνθήκες
-						echo '
-						<li class="nav-item">
-							<a class="nav-link " href=""></a>
-						</li>';
-					}
-					?>
-					
-
-
-                  
+                <ul class="navbar-nav">
+                  <li class="nav-item"><a class="nav-link" href="<?php echo BASE_URL; ?>index.php">Home</a></li>
+                  <li class="nav-item"><a class="nav-link" href="<?php echo BASE_URL; ?>calculation_istor.php">ιστορικο</a></li>
+                  <li class="nav-item"><a class="nav-link" href="<?php echo BASE_URL; ?>calculation.php">υπολογισμοι</a></li>
+                  <li class="nav-item"><a class="nav-link" href="<?php echo BASE_URL; ?>blog.php">αρθρα</a></li>
+                  <li class="nav-item"><a class="nav-link" href="<?php echo BASE_URL; ?>doctors.php">Doctors</a></li>
+                  <li class="nav-item"><a class="nav-link" href="<?php echo BASE_URL; ?>charts.php">Διαγραμματα</a></li>
+                  <?php
+                    if (isset($_SESSION['consultant']) && $_SESSION['consultant'] !== '0' && !empty($_SESSION['consultant'])) {
+                      echo '<li class="nav-item"><a class="nav-link" href="' . BASE_URL . 'your_doctor.php">Ο ΓΙΑΤΡΟΣ ΣΟΥ</a></li>';
+                    } else if ((isset($_SESSION['email']) && isset($_SESSION['role'])) && $_SESSION['role'] == 'visitor') {
+                      echo '<li class="nav-item"><a class="nav-link" href="' . BASE_URL . 'choose_doctor.php">ΔΙΑΛΕΞΕ ΓΙΑΤΡΟ</a></li>';
+                    } else if ((isset($_SESSION['email']) && isset($_SESSION['role'])) && $_SESSION['role'] == 'Doctor') {
+                      echo '<li class="nav-item"><a class="nav-link" href="' . BASE_URL . 'teleconfer_con.php?consultant=' . $_SESSION['id_user'] . '">ΤΗΛΕΙΑΤΡΙΚΗ</a></li>';
+                    }
+                  ?>
                 </ul>
               </div>
-			  <div class="quote_btn-container">
-			  <?php 
-				if(!isset($_SESSION['email'])){
-                echo '<a  href="login.php">
-                  <i class="fa fa-user" aria-hidden="true"></i>
-                  <span>
-                    Login
-                  </span>
-                </a>';}
-				else{
-					echo '<a  href="logout.php">
-                  <i class="fa fa-user" aria-hidden="true"></i>
-                  <span>
-                    Logout
-                  </span>
-                </a>';
-				} ?>
-				<?php 
-				if(!isset($_SESSION['email'])){
-                echo '<a  href="register.php">
-                  <i class="fa fa-user" aria-hidden="true"></i>
-                  <span>
-                    Sign Up
-                  </span>
-                </a>';}
-				else {if ( isset($_SESSION['role']) && $_SESSION['role'] == 'visitor') {
-						// Αν ο χρήστης είναι επισκέπτης
-						echo '
-						<i aria-hidden="true">
-						
-							<a class="  " href="my_data.php">YOUR DATA</a>
-							
-						</i>';
-					} else if ( isset($_SESSION['role']) && $_SESSION['role'] == 'Doctor') {
-						// Αν ο χρήστης είναι γιατρός
-						echo '
-						<i aria-hidden="true">
-						<span>
-							<a class=" " href="patiens.php">ΑΣΘΕΝΕΙΣ ΣΟΥ</a>
-						</span>	
-						</i>';
-					}
-					  } ?>
-				<i aria-hidden="true">
-						<span>
-							<a class=" " href="statistics.php">STAT</a>
-						</span>	
-						</i>
-				
-				</div>
-              
+
+              <div class="quote_btn-container">
+                <?php 
+                  if(!isset($_SESSION['email'])){
+                    echo '<a href="' . BASE_URL . 'login.php"><i class="fa fa-user"></i><span>Login</span></a>';
+                  } else {
+                    echo '<a href="' . BASE_URL . 'logout.php"><i class="fa fa-user"></i><span>Logout</span></a>';
+                  }
+
+                  if(!isset($_SESSION['email'])){
+                    echo '<a href="' . BASE_URL . 'register.php"><i class="fa fa-user"></i><span>Sign Up</span></a>';
+                  } else {
+                    if (isset($_SESSION['role']) && $_SESSION['role'] == 'visitor') {
+                      echo '<i><a class="" href="' . BASE_URL . 'my_data.php">YOUR DATA</a></i>';
+                    } else if (isset($_SESSION['role']) && $_SESSION['role'] == 'Doctor') {
+                      echo '<i><span><a class="" href="' . BASE_URL . 'patiens.php">ΑΣΘΕΝΕΙΣ ΣΟΥ</a></span></i>';
+                    }
+                  }
+                ?>
+                <i><span><a class="" href="<?php echo BASE_URL; ?>auxi/statistics.php">STAT</a></span></i>
+              </div>
             </div>
           </nav>
         </div>
       </div>
     </header>
-    <!-- end header section -->
   </div>
