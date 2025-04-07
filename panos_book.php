bb816d1ac625915dc707c563a6558ca2218cd87c
Nikos Gavalakis <nikosgavalakis@gmail.com>
2025-04-07 09:56:12 +0300

    Delete panos_book.php

--- panos_book.php
+++
@@ -1,8 +0,0 @@
-<?php include('up.php'); ?>
-
-<!-- Ο προγραμματισμός ραντεβού στο Ημερολόγιο Google ξεκινά -->
-<iframe src=" https://calendar.google.com/calendar/appointments/schedules/AcZssZ2AlrNMOHorQdtyc17JTAKQ7eKsYFUN-4t-90w87sNqAuZoEZ_7fM_q6ZyNE-IuSZK5QOBkiuRc?gv=true "? 0" width="100%" height="600" frameborder="0"></iframe>
-<!-- τέλος Προγραμματισμός ραντεβού στο Ημερολόγιο Google -->
-
-
-<?php include('down.php'); ?>
