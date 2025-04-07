ab6d11fbe607433e6828d387539615f6c34fc161
Nikos Gavalakis <nikosgavalakis@gmail.com>
2025-04-07 09:49:18 +0300

    Add files via upload

---
+++ biblio/reviews.php
@@ -0,0 +1,24 @@
+<?php include('../up.php'); ?>
+<br>
+<div  style="display: flex;
+            justify-content: center;
+            align-items: center;
+            margin: 0;">
+
+<div class="pdf-container">
+        <iframe src="<?php echo BASE_URL; ?>files/reviews of chatbots.pdf"></iframe>
+    </div>
+
+</div>
+
+<div  style="display: flex;
+            justify-content: center;
+            align-items: center;
+            margin: 0;"><br><br><br>
+<button style="background-color: rgb(162,235,182); padding: 10px 20px; border: none; border-radius: 5px; cursor: pointer;">
+  <a href="<?php echo BASE_URL; ?>bibliography.php" style="text-decoration: none; color: black;">Πίσω</a>
+ </button>
+ </div>
+
+
+<?php include('../down.php'); ?>
