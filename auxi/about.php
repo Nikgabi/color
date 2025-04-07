a79c9a0482237f7355036a33bd692ba8f68268ef
Nikos Gavalakis <nikosgavalakis@gmail.com>
2025-04-07 13:41:14 +0300

    Add files via upload

---
+++ auxi/about.php
@@ -0,0 +1,49 @@
+<?php include('../up.php'); ?>
+
+<head>
+	<title>About me</title>
+</head>
+
+<section class="about_section"><br>
+    <div class="container  ">
+      <div class="row">
+        <div class="col-md-3 ">
+          <div class="img-box">
+            <img src="<?php echo BASE_URL; ?>images/16.jpeg" alt="">
+          </div><br>
+		  <div class="img-box">
+            <img src="<?php echo BASE_URL; ?>images/about.jpg" alt="">
+          </div>
+        </div>
+        <div class="col-md-9">
+          <div class="detail-box">
+            <div class="heading_container">
+              <h2>
+                Το "ygeiafirst.net" είναι η Πτυχιακή μου.
+				<span>Τα menu μας : </span>
+              </h2>
+            </div>
+			<p><strong>Ιστορικό:</strong> Εισαγωγή ιατρικού ιστορικού και εξετάσεών σας. Ο φάκελος υγείας σας.</p>
+            <p><strong>Υπολογισμοί:</strong> Ιατρικοι υπολογισμοί και εκτιμήσεις κινδύνων για ασθένειες.</p>
+				<p><strong>Άρθρα:</strong> Άρθρα ιατρικά γενικού ενδιαφέροντος από τους εγγεγραμμένους γιατρούς μας.</p>
+				
+				<p><strong>Doctors:</strong> Οι γιατροί μας. Δείτε πληροφορίες τους από το Linkedin , webpage και τα κοινωνικά τους δίκτυα.</p>
+				<p><strong>Βιβλιογραφία:</strong> Άρθρα και Ιστότοποι σχετικά με Ιατρικούς υπολογισμούς ,ιατρικές πληροφορίες και ιατρικές εφαρμογές.</p>
+				<p><strong>Διαγράμματα:</strong>Δείτε διαγράμματα από τα δεδομένα σας ή των ασθενών σας συγκριτικά με το μ.ο. όλων</p>
+				<p><strong>Testimonials:</strong> Πείτε μας την γνώμη σας για τον ιστότοπό μας.</p>
+				<p><strong>Διάλεξε Γιατρό, Ο Γιατρός σου, Your Data:</strong> Για τους εγγεγραμένους επισκέπτες.</p>
+				<p><strong>Οι ασθενείς σου:</strong> Για τους εγγεγραμένους γιατρούς.</p>
+				<p>Ο ιστότοπος αυτός δεν έχει ευθύνη για τις ιατρικές πληροφορίες που αναζητάτε και είναι πληροφοριακού χαρακτήρα και μόνο.
+				Πρέπει να έχετε κάνει εγγραφή ώς επισκέπτης για να έχετε πρόσβαση παντού.</p><br>
+				<strong> Για ιατρική συμβουλή και θεραπεία απευθύνεστε στους ειδικούς γιατρούς που εμπιστεύεστε.</strong>
+            
+			
+          </div>
+        </div>
+      </div><br><br>
+    </div>
+  </section>
+
+</body>
+
+<?php include('../down.php'); ?>
