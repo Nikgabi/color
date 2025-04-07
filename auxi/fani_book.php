b959a6bbea6b7950342dd925d40fc77fddc464be
Nikos Gavalakis <nikosgavalakis@gmail.com>
2025-04-07 09:48:06 +0300

    Add files via upload

---
+++ auxi/fani_book.php
@@ -0,0 +1,9 @@
+<?php include('../up.php'); ?>
+
+<!-- Ο προγραμματισμός ραντεβού στο Ημερολόγιο Google ξεκινά -->
+<iframe src=" https://calendar.google.com/calendar/appointments/schedules/AcZssZ3LSv5T8rS2seIJ6MdKm2NN_xOqjL1ObanSOgZUTEWSdobvX_LvqgeTPM_eZRO00CyJYgKcUKkZ?gv=true style="e width="100%" height="600" frameborder="0"></iframe>
+<!-- τέλος Προγραμματισμός ραντεβού στο Ημερολόγιο Google -->
+
+
+
+<?php include('../down.php'); ?>
