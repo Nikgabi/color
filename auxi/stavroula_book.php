e2eb75ca2ed72b91d7a75e33dc3338d3e90d090d
Nikos Gavalakis <nikosgavalakis@gmail.com>
2025-04-08 17:11:52 +0300

    Add files via upload

---
+++ auxi/stavroula_book.php
@@ -0,0 +1,7 @@
+<?php include('../up.php'); ?>
+
+<!-- Ο προγραμματισμός ραντεβού στο Ημερολόγιο Google ξεκινά -->
+<iframe src=" https://calendar.google.com/calendar/appointments/schedules/AcZssZ1Q9vTd0vqH4yiUCTFjuJXyh3TEPheYU74qq2O4QcnFxu907M80mL1U0IG7RtLG_DwRcBncttnX?gv=true " style="border: 0" width="100%" height="600" frameborder="0"></iframe>
+<!-- τέλος Προγραμματισμός ραντεβού στο Ημερολόγιο Google -->
+
+<?php include('../down.php'); ?>
