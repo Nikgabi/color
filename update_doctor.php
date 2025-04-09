19bcb02c35b5c1a0a5e6ac7bd332249b1154d4b1
Nikos Gavalakis <nikosgavalakis@gmail.com>
2025-04-09 20:47:39 +0300

    Add files via upload

--- update_doctor.php
+++ update_doctor.php
@@ -1,28 +1,37 @@
 
 
 <?php 
-	session_start();
-	include ('connection.php');
-	if (!isset($_SESSION['id_user'])) {
+session_start();
+require_once('connection.php'); // καλύτερα από include για να μην φορτωθεί ξανά κατά λάθος
+
+if (!isset($_SESSION['id_user'])) {
     die("Σφάλμα: Δεν υπάρχει ενεργός χρήστης.");
-	}
+}
 
-	$user = $_SESSION['id_user'];
+$user = intval($_SESSION['id_user']); // ασφάλεια: μετατρέπουμε σε ακέραιο
 
-	// Εκτέλεση του query με έλεγχο σφαλμάτων
-	$query = "UPDATE user SET consultant='',sygkat='' WHERE id_user= ?";
-	$stmt = mysqli_prepare($con, $query);
-	mysqli_stmt_bind_param($stmt, "i", $user);
+// Προετοιμασία και εκτέλεση του query
+$query = "UPDATE user SET consultant = '', sygkat = '' WHERE id_user = ?";
+$stmt = mysqli_prepare($con, $query);
 
-	if (mysqli_stmt_execute($stmt)) {
-		header("Location: menu/choose_doctor.php");
-		exit();
-	} else {
-		echo "Σφάλμα στην εκτέλεση του query.";
-	}
+if ($stmt === false) {
+    die("Σφάλμα στην προετοιμασία του query: " . mysqli_error($con));
+}
 
+mysqli_stmt_bind_param($stmt, "i", $user);
 
+if (mysqli_stmt_execute($stmt)) {
+    mysqli_stmt_close($stmt);
+    mysqli_close($con);
+    header("Location: menu/choose_doctor.php");
+    exit();
+} else {
+    echo "Σφάλμα στην εκτέλεση του query: " . mysqli_stmt_error($stmt);
+    mysqli_stmt_close($stmt);
+    mysqli_close($con);
+}
 ?>
+
 
 
 
