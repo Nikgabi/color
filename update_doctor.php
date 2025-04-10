d845cd16f04f91aba83e2705c722cf2036374b2d
Nikos Gavalakis <nikosgavalakis@gmail.com>
2025-04-10 09:41:41 +0300

    Add files via upload

--- update_doctor.php
+++ update_doctor.php
@@ -7,14 +7,24 @@ if (!isset($_SESSION['id_user'])) {
 $user = intval($_SESSION['id_user']); // ασφάλεια: μετατρέπουμε σε ακέραιο
 
 // Προετοιμασία και εκτέλεση του query
-$query = "UPDATE user SET consultant = '', sygkat = '' WHERE id_user = ?";
+$query = "UPDATE user SET consultant = '', sygkat = '' WHERE id_user = $user";
 $stmt = mysqli_prepare($con, $query);
 
 if ($stmt === false) {
     die("Σφάλμα στην προετοιμασία του query: " . mysqli_error($con));
 }
 
-mysqli_stmt_bind_param($stmt, "i", $user);
+
+if (!isset($_SESSION['id_user'])) {
+    die("Σφάλμα: Δεν υπάρχει ενεργός χρήστης.");
+}
+
+
+// Εκτέλεση
+if (!mysqli_stmt_execute($stmt)) {
+    die("❌ Σφάλμα στην εκτέλεση του query: " . mysqli_stmt_error($stmt));
+}
+
 
 if (mysqli_stmt_execute($stmt)) {
 	echo "Redirecting...<br>";
