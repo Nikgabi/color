1db1282ee9d43ff25082487ea54c6eb7d80fff44
Nikos Gavalakis <nikosgavalakis@gmail.com>
2025-04-14 21:53:46 +0300

    Add files via upload

---
+++ auxi/george_book.php
@@ -0,0 +1,9 @@
+<?php include('../up.php'); ?>
+
+<!-- Google Calendar Appointment Scheduling begin -->
+<iframe src="https://calendar.google.com/calendar/appointments/schedules/AcZssZ0yglS7DZYQUED0P-mF0qgEqAXFCm0Wxfdys7r9IawF0PVo5et2e_u-dll5VJu4OhVlg0INi3su?gv=true" style="border: 0" width="100%" height="600" frameborder="0"></iframe>
+<!-- end Google Calendar Appointment Scheduling -->
+
+
+
+<?php include('../down.php'); ?>
